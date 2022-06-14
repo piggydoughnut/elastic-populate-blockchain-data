7637cb71105c95c3f440b462f8ebdb8502961d5f
piggydoughnut <piggydoughnut@gmail.com>
2022-06-14 11:56:08 +0800

    init

---
+++ index.ts
@@ -0,0 +1,34 @@
+import SmartChain from "komodo-rpc-js";
+
+const config = {
+  rpchost: "localhost",
+  rpcport: 7771,
+  rpcuser: process.env.RPC_USER,
+  rpcpassword: process.env.RPC_PASS
+};
+ 
+const tokel = new SmartChain({ config });
+ 
+console.log(tokel.config); // Prints the config being used by the komodo instance
+ 
+const tokelRPC = tokel.rpc();
+ 
+tokelRPC
+  .getinfo()
+  .then(info => {
+    console.log(info);
+  })
+  .catch(error => {
+    console.log(error);
+    throw new Error(error);
+  });
+ 
+tokelRPC
+  .listunspent(6, 9999999, [
+    "RPS3xTZCzr6aQfoMw5Bu1rpQBF6iVCWsyu",
+    "RBtNBJjWKVKPFG4To5Yce9TWWmc2AenzfZ"
+  ])
+  .then(outs => {
+    console.log(outs);
+  })
+  .catch(error => console.log(error));
